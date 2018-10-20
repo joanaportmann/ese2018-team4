8bb68bacd7d261f4a31a2a1e0642a5e700b54ee8
Joana Portmann <joana_p@windowslive.com>
2018-10-20 20:01:51 +0200

    Start working on passport authentication

--- backend/app/controllers/job.controller.ts
+++ backend/app/controllers/job.controller.ts
@@ -1,19 +1,25 @@
 import {Router, Request, Response} from 'express';
 import {Job} from '../models/job.model';
+import passport from 'passport';
 
 const router: Router = Router();
+
 router.get('/', async (req: Request, res: Response) => {
   const instances = await Job.findAll();
   res.statusCode = 200;
   res.send(instances.map(job => job.toSimplification()));
 });
+
 router.post('/', async (req: Request, res: Response) => {
+  passport.authenticate('local');
+
   const instance = new Job();
   instance.fromSimplification(req.body);
   await instance.save();
   res.statusCode = 201;
   res.send(instance.toSimplification());
 });
+
 router.get('/:id', async (req: Request, res: Response) => {
   const id = parseInt(req.params.id);
   const instance = await Job.findById(id);
@@ -27,6 +33,7 @@ router.get('/:id', async (req: Request, res: Response) => {
   res.statusCode = 200;
   res.send(instance.toSimplification());
 });
+
 router.put('/:id', async (req: Request, res: Response) => {
   const id = parseInt(req.params.id);
   const instance: Job | null = await Job.findById(id);
@@ -42,6 +49,7 @@ router.put('/:id', async (req: Request, res: Response) => {
   res.statusCode = 200;
   res.send(instance.toSimplification());
 });
+
 router.delete('/:id', async (req: Request, res: Response) => {
   const id = parseInt(req.params.id);
   const instance = await Job.findById(id);
