743b27da72aaedda0435353829890822a9805e1b
Ann Kiener <ann.kiener@students.unibe.ch>
2018-10-21 16:20:21 +0200

    added formControl to required fields

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Job} from './job';
 import {HttpClient} from '@angular/common/http';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,15 @@ export class AppComponent implements OnInit {
   job: Job = new Job(null, '', '', '');
   jobs: Job[] = [];
   Validators
+  nameFormControl = new FormControl('', [
+    Validators.required
+  ]);
+  matcher = new MyErrorStateMatcher();
+  descriptionFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  matcher2 = new MyErrorStateMatcher();
 
   constructor(private httpClient: HttpClient) {
 
@@ -20,7 +30,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.httpClient.get('http://localhost:3000/job').subscribe((instances: any) => {
       this.jobs = instances.map((instance) =>
-          new Job(instance.id, instance.name, instance.description, instance.necessarySkills));
+        new Job(instance.id, instance.name, instance.description, instance.necessarySkills));
     });
   }
 
@@ -37,3 +47,12 @@ export class AppComponent implements OnInit {
     this.jobs.splice(this.jobs.indexOf(job), 1);
   }
 }
+  export class MyErrorStateMatcher implements ErrorStateMatcher {
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+      const isSubmitted = form && form.submitted;
+      return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    }
+  }
+
+
+
