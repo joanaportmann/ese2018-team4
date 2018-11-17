47dbc559905aab50970115a4c60050d43045a559
Joana Portmann <joana_p@windowslive.com>
2018-11-17 19:39:39 +0100

    change job table in db

--- backend/app/controllers/job.controller.ts
+++ backend/app/controllers/job.controller.ts
@@ -1,5 +1,5 @@
-import {Router, Request, Response} from 'express';
-import {Job} from '../models/job.model';
+import { Router, Request, Response } from 'express';
+import { Job } from '../models/job.model';
 
 const router: Router = Router();
 
@@ -19,6 +19,7 @@ router.post('/', (req: Request, res: Response, next: Function) => {
   const instance = new Job();
 
   instance.fromSimplification(req.body);
+  instance.approved = false;
   await instance.save();
   res.statusCode = 201;
   res.send(instance.toSimplification());
@@ -39,6 +40,7 @@ router.get('/:id', async (req: Request, res: Response) => {
 });
 
 router.put('/:id', async (req: Request, res: Response) => {
+  console.log('not good.....................................');
   const id = parseInt(req.params.id);
   const instance: Job | null = await Job.findById(id);
   if (instance == null) {
@@ -68,6 +70,29 @@ router.delete('/:id', async (req: Request, res: Response) => {
   await instance.destroy();
   res.statusCode = 204;
   res.send();
+});
+
+const approveRouter: Router = Router({ mergeParams: true });
+router.use('/:id/approved', approveRouter);
+
+approveRouter.put('/', async (req: Request, res: Response) => {
+  console.log('WHHHHHHHHHHHHHHHHHAAAAAAAAAAAATTT');
+  console.log(req.body);
+  const id = parseInt(req.params.id);
+  const instance: Job | null = await Job.findById(id);
+  if (instance == null) {
+    res.statusCode = 404;
+    res.json({
+      'message': 'not found'
+    });
+    return;
+  }
+  instance.approved = req.body;
+  await instance.save();
+  res.statusCode = 200;
+  res.json({
+    'message': 'job enabled'
+  });
 });
 
 export const JobController: Router = router;
