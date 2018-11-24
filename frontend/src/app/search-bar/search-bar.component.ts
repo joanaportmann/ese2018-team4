f91cf7549e9a890f270fc7944237e2f86a237164
Joel Zuber <joel.zuber@students.unibe.ch>
2018-11-24 10:22:52 +0100

    Trimmer

--- frontend/src/app/search-bar/search-bar.component.ts
+++ frontend/src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {Job} from '../models/job';
+import {catchError, tap} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
   }
 
+  search(text: string): Observable<Job[]> {
+    if (!text.trim()) {
+      // no text
+      return of([]);
+    }
+    return this.http.get<Job[]>(`${this.jobsUrl}/?name=${text}`).pipe(
+      tap(_ => this.log()),
+      catchError(this.handleError<Job[]>('search', []))
+    );
+  }
+
+  private log() {}
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log();
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
