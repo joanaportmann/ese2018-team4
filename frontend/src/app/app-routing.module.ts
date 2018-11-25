e22464a6a66a54154dc21128feed1ee0fc082058
Joel Zuber <joel.zuber@students.unibe.ch>
2018-11-25 19:42:48 +0100

    Create route to search

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {SearchBarComponent} from './search-bar/search-bar.component';
+import {SearchComponent} from './search/search.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: SearchBarComponent }
+  { path: 'home', component: SearchBarComponent },
+  { path: 'search/:text', component: SearchComponent }
 ];
 
 @NgModule({
