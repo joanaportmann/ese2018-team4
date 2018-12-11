ab46e826c6c68ffd6106da073e57565185469fa5
Joel Zuber <joel.zuber@students.unibe.ch>
2018-12-11 17:11:50 +0100

    Solve merging problems

--- frontend/src/app/search-bar/search-bar.component.ts
+++ frontend/src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor() {}
+  input = '';
+
+  constructor(private router: Router) {
+    this.input = window.location.pathname.substr(8);
+  }
 
   search() {
     this.router.navigateByUrl('/demand/' + this.input);
