e969deb537119f3639e3d12fa9cd93de3b1ef3d4
Joel Zuber <joel.zuber@students.unibe.ch>
2018-12-11 16:55:18 +0100

    Add brackets

--- frontend/src/app/search-bar/search-bar.component.ts
+++ frontend/src/app/search-bar/search-bar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor(
+  constructor() {}
 
   search() {
     this.router.navigateByUrl('/demand/' + this.input);
